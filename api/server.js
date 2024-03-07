a080f24210ad92df901eeeb491ad7fb2108c905a
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2024-03-07 16:41:00 +0700

    install mysql2
    npm i mysql2

--- api/server.js
+++ api/server.js
@@ -5,9 +5,9 @@ var app = express()
 app.use(cors())
 
 app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+  res.json({msg: 'hello world'})
 })
 
 app.listen(5000, function () {
-  console.log('CORS-enabled web server listening on port 80')
+  console.log('CORS-enabled web server listening on port 5000')
 })
