9e33103e7853b3e443382e74d374c8666d99ca49
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2024-03-07 17:00:25 +0700

    Update server.js
    Callback
    Create the connection to database
    simple SELECT query

--- api/server.js
+++ api/server.js
@@ -1,13 +1,31 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
+var express = require("express");
+var cors = require("cors");
+var app = express();
 
-app.use(cors())
+//Callback
+// Get the client
+const mysql = require("mysql2");
 
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'hello world'})
-})
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root2",  
+  database: "travel",
+});
+
+app.use(cors());
+
+app.get("/api/attractions", function (req, res, next) {
+  // A simple SELECT query
+  connection.query(
+    "SELECT * FROM `attractions` LIMIT 0, 10",
+    function (err, results, fields) {
+      console.log(results);
+      res.json({ results: results });
+    }
+  );
+});
 
 app.listen(5000, function () {
-  console.log('CORS-enabled web server listening on port 5000')
-})
+  console.log("CORS-enabled web server listening on port 5000");
+});
