e7623b53cdcf95388e7ee96cdb8faae46f8396d9
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2024-03-07 16:38:53 +0700

    Update server.js
    install nodemon
    use port 5000

--- api/server.js
+++ api/server.js
@@ -8,6 +8,6 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
-app.listen(80, function () {
+app.listen(5000, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
